3fd6b7c2ae4f80d303de87cb641c2b1c6794a4a1
Sabrina St <git@s-steinhardt.de>
2023-09-08 11:46:14 +0200

    implemented todos

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"html/template"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -16,9 +19,20 @@ type Project struct {
 }
 
 // 2) TODO: Create a Network struct to hold the data for each network
+type Network struct {
+	Name string `json:"Name"`
+	Link string `json:"Link"`
+}
 
 // 3) TODO: Create a Portfolio struct to hold the data for the entire portfolio
 // Hint: Notice that the attributs "Projects" and "Networks" must be slices/lists
+type Portfolio struct {
+	Name     string    `json:"Name"`
+	Title    string    `json:"Title"`
+	About    string    `json:"About"`
+	Projects []Project `json:"Projects"`
+	Networks []Network `json:"Networks"`
+}
 
 // 4) TODO: Go into templates/portfolio.gohtml and call the data from the Portfolio struct everywhere it says "TODO"
 
@@ -44,21 +58,38 @@ func handleData() Portfolio {
 
 	return portfolio
 }
-
 func createHTML(portfolio Portfolio) {
-	// TODO: Create the static/index.html file
+	// TODO: Create the index.html file
+	file, err := os.Create("index.html")
+	if err != nil {
+		log.Fatal("Failed to create index.html file:", err)
+	}
+	defer file.Close()
 
 	// TODO: Parse the template file
+	tmpl, err := template.ParseFiles("templates/portfolio.gohtml")
+	if err != nil {
+		log.Fatal("Failed to parse template file:", err)
+	}
 
 	// TODO: Execute the template and write the output to the index.html
-
+	err = tmpl.Execute(file, portfolio)
+	if err != nil {
+		log.Fatal("Failed to execute template:", err)
+	}
 }
 
 func startServer() {
 	// TODO: Create a file server to serve the files in the static directory
+	fs := http.FileServer(http.Dir("."))
 
 	// TODO: Handle the root path "/"
+	http.Handle("/", fs)
 
 	// TODO: Start the server, listen on port 8080
-
+	fmt.Println("Server listening on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
